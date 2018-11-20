6443c810fa92e8c6a7907091a77dddd81d9243ed
tregohex <de.nisi98@live.com>
2018-11-20 20:15:41 +0100

    COLLISION (E05)

---
+++ Cube_Game_Course/Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovements movements;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit something");
+            movements.enabled = false;
+        }
+        
+    }
+}
---
+++ Cube_Game_Course/Assets/PlayerMovements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+	// Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime ,0,0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
