9cd9965e3832dd5f98b07ea3e757e3e39bf50b55
tregohex <de.nisi98@live.com>
2018-11-29 14:56:38 +0100

    character controller
    Script changed

---
+++ Cube_Game_Course/Assets/Scripts/v2MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class v2MovementScript : MonoBehaviour
+{
+    public Collision collisionInfo = new Collision();
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    private void Start()
+    {
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
