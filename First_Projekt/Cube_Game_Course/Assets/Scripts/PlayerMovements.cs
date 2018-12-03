b618c34dc026d90f8941edb67ab43d6f7332bb07
hextrego <de.nisi98@live.com>
2018-12-03 16:23:16 +0100

    Added Firebase Example Folder

---
+++ First_Projekt/Cube_Game_Course/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float upwardForce = 600f;
+    public float speed;
+    public Collision collisionInfo = new Collision();
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+
+        
+    }
+  
+
+}
