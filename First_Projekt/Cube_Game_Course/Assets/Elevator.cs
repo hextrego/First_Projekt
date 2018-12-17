43b36db16095801a68c7b223cbd723bc8442e3cd
hextrego <de.nisi98@live.com>
2018-12-17 17:32:03 +0100

    Added Elevator

---
+++ First_Projekt/Cube_Game_Course/Assets/Elevator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public GameObject player;
+    public float x = 0, y = 2, z  =0.42F;
+
+    // The target marker.
+    public Transform target;
+
+    // Speed in units per sec.
+    public float speed;
+    // Use this for initialization
+
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+            // The step size is equal to speed times frame time.
+            float step = speed * Time.deltaTime;
+
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            //player.transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
